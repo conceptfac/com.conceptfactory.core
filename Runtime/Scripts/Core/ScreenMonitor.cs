4cbf3c3e98a8885eaca6a35d58abb24c2e09e069
Twinny VR <dev@twinnyvr.com.br>
2025-05-15 17:43:49 -0300

    Old avatars removed from Platform Scene

--- Runtime/Scripts/Core/ScreenMonitor.cs
+++ Runtime/Scripts/Core/ScreenMonitor.cs
@@ -6,31 +6,31 @@ using UnityEngine.Events;
 namespace Concept.Core
 {
 
-[Serializable] public class OnLandscapeOrientationEvent : UnityEvent { }
-[Serializable] public class OnPortraitOrientationEvent : UnityEvent { }
+    [Serializable] public class OnLandscapeOrientationEvent : UnityEvent { }
+    [Serializable] public class OnPortraitOrientationEvent : UnityEvent { }
 
 
-public class ScreenMonitor : TSingleton<ScreenMonitor>
-{
-    private ScreenOrientation _lastScreenOrientation;
+    public class ScreenMonitor : TSingleton<ScreenMonitor>
+    {
+        private ScreenOrientation _lastScreenOrientation;
 
-    #region Delegates
-    public delegate void onResolutionChanged(int width, int height);
-    public static onResolutionChanged OnResolutionChanged;
+        #region Delegates
+        public delegate void onResolutionChanged(int width, int height);
+        public static onResolutionChanged OnResolutionChanged;
         #endregion
 
 
         protected override void Update()
         {
             base.Update();
-        if (_lastScreenOrientation != Screen.orientation)
-        {
-            _lastScreenOrientation = Screen.orientation;
-            OnResolutionChanged?.Invoke(Screen.width, Screen.height);
-        }
+            if (_lastScreenOrientation != Screen.orientation)
+            {
+                _lastScreenOrientation = Screen.orientation;
+                OnResolutionChanged?.Invoke(Screen.width, Screen.height);
+            }
 
+        }
     }
-}
 
 
 }
