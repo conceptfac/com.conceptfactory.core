313f8a6d2c786e8032315e80189215e1436146b3
Twinny VR <dev@twinnyvr.com.br>
2025-09-16 16:08:17 -0300

    Vector2RangeDrawer Fixed

--- Editor/Vector2RangeDrawer.cs
+++ Editor/Vector2RangeDrawer.cs
@@ -1,23 +1,75 @@
-using Concept.Helpers;
 using UnityEditor;
 using UnityEngine;
 
-namespace Concept.Editor
+[CustomPropertyDrawer(typeof(Vector2RangeAttribute))]
+public class Vector2RangeDrawer : PropertyDrawer
 {
-    [CustomPropertyDrawer(typeof(Vector2Range))]
-    public class Vector2RangeDrawer : PropertyDrawer
+    const float kSpacing = 2f;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        if (property.propertyType != SerializedPropertyType.Vector2)
         {
-            EditorGUI.BeginProperty(position, label, property);
-            SerializedProperty x = property.FindPropertyRelative("x");
-            SerializedProperty y = property.FindPropertyRelative("y");
-
-            float width = position.width / 2;
-            EditorGUI.Slider(new Rect(position.x, position.y, width - 2, position.height), x, 0f, 1f, new GUIContent("X"));
-            EditorGUI.Slider(new Rect(position.x + width + 2, position.y, width - 2, position.height), y, 0f, 1f, new GUIContent("Y"));
-
-            EditorGUI.EndProperty();
+            EditorGUI.LabelField(position, label.text, "Use Vector2 with Vector2Range");
+            return;
         }
+
+        Vector2RangeAttribute range = (Vector2RangeAttribute)attribute;
+        Vector2 value = property.vector2Value;
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        float line = EditorGUIUtility.singleLineHeight;
+        float labelWidth = EditorGUIUtility.labelWidth;
+        float contentWidth = position.width - labelWidth;
+        float x = position.x;
+        float y = position.y;
+
+        // --- LABEL (nome da propriedade) ---
+        Rect labelRect = new Rect(x, y + line + kSpacing, labelWidth, line); // empurra pro meio
+        GUIStyle middleLeft = new GUIStyle(EditorStyles.label)
+        {
+            alignment = TextAnchor.MiddleLeft
+        };
+        EditorGUI.LabelField(labelRect, label, middleLeft);
+
+        // --- VALORES (em cima do slider) ---
+        Rect topValuesRect = new Rect(x + labelWidth, y, contentWidth, line);
+        GUIStyle miniLeft = new GUIStyle(EditorStyles.miniLabel) { alignment = TextAnchor.MiddleLeft };
+        GUIStyle miniRight = new GUIStyle(EditorStyles.miniLabel) { alignment = TextAnchor.MiddleRight };
+
+        EditorGUI.LabelField(
+            new Rect(topValuesRect.x, topValuesRect.y, topValuesRect.width * 0.5f, topValuesRect.height),
+            value.x.ToString("0.00"), miniLeft);
+        EditorGUI.LabelField(
+            new Rect(topValuesRect.x + topValuesRect.width * 0.5f, topValuesRect.y,
+                     topValuesRect.width * 0.5f, topValuesRect.height),
+            value.y.ToString("0.00"), miniRight);
+
+        // --- SLIDER ---
+        Rect sliderRect = new Rect(x + labelWidth, y + line + kSpacing, contentWidth, line);
+        EditorGUI.MinMaxSlider(sliderRect, ref value.x, ref value.y, range.min, range.max);
+        value.x = Mathf.Clamp(value.x, range.min, value.y);
+        value.y = Mathf.Clamp(value.y, value.x, range.max);
+
+        // --- LABELS FIXOS Min/Max (embaixo) ---
+        Rect bottomLabelsRect = new Rect(x + labelWidth, y + line * 2 + kSpacing * 2, contentWidth, line);
+        EditorGUI.LabelField(
+            new Rect(bottomLabelsRect.x, bottomLabelsRect.y, bottomLabelsRect.width * 0.5f, bottomLabelsRect.height),
+            "Min", miniLeft);
+        EditorGUI.LabelField(
+            new Rect(bottomLabelsRect.x + bottomLabelsRect.width * 0.5f, bottomLabelsRect.y,
+                     bottomLabelsRect.width * 0.5f, bottomLabelsRect.height),
+            "Max", miniRight);
+
+        property.vector2Value = value;
+
+        EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        // altura total
+        return EditorGUIUtility.singleLineHeight * 3 + kSpacing * 3;
     }
 }
--- Runtime/Scripts/Helpers/Vector2Range.cs
+++ Runtime/Scripts/Helpers/Vector2Range.cs
@@ -1,17 +1,13 @@
 using UnityEngine;
 
-namespace Concept.Helpers
+public class Vector2RangeAttribute : PropertyAttribute
 {
-    [System.Serializable]
-    public struct Vector2Range
-    {
-        public float x; // min
-        public float y; // max
+    public float min;
+    public float max;
 
-        public Vector2Range(float min, float max)
-        {
-            x = min;
-            y = max;
-        }
+    public Vector2RangeAttribute(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
     }
 }
